90075037c545757c40f4344b5b4622043bf0010e
M Rumman Hasan <mrayyanhkhan@outlook.com>
2018-11-15 22:46:03 +0500

    changed approach with simplified code

--- aidisease.py
+++ aidisease.py
@@ -1,103 +1,37 @@
 import io
 
 # MainSymp : symp1, symp2....
-listAllsymp = [['   Deep Cough   ',  '  Ear Pain  ',  '  Sore Swelling  ',  '  Change in Voice  ',  '   Difficulty in breathing   '],
-               ['   Jaw Pain   ',  '  Sore Throat  ',  '  Teeth Loose  ',  '  Painful Chewing  '],
-               ['   MainSymp3   ',  '  weakness  ',  '   fever   ','   cheek swelling   ',  '   other   '],
-               ['   MainSymp4   ',  '   s1   ',  '   s2   ']]
+listofAllsymp = {
+    "Deep Cough": ["ear Pain", "sore Swelling", "change in Voice", "difficulty in breathing"],
+    "Jaw Pain": ["sore Throat", "teeth Loose", "painful Chewing"],
+    "MainSymp3": ["weakness", "fever", "cheek swelling", "other", "other2"],
+    "MainSymp4": ["s1", "s2", "s3"],
+    "MainSymp5": ["s1", "s2", "s3"],
+    "MainSymp6": ["s1", "s2", "s3", "s4"]
+}
 
-for i in listAllsymp:
-    for j in i:
-        print(j)
+# Pick
+QuesList = []
+for ms,ns in listofAllsymp.items():
+    QuesList.append(ms)
+# print(QuesList,"\n")
+# Output: QuesList = ['Deep Cough', 'Jaw Pain', 'MainSymp3', 'MainSymp4', 'MainSymp5', 'MainSymp6']
 
-# Populate MainSymptoms
-# Output : [['Deep Cough'], ['Jaw Pain'], ['MainSymp3'], ['MainSymp4']]
-quesList = []
-i = 0
-for ms in listAllsymp:
-    # print(listAllsymp[i][0:1])
-    quesList.append(listAllsymp[i][0:1])
-    i = i+1
-print(quesList)
-# print()
-
-ms = 0
-aaa = []
-print(ms)
-aaa.append(str(listAllsymp[ms][0]))
-index = quesList.index(aaa)  # index of MainSymp
-print(index)
-
-eachdataset = 0
-for ques in quesList:
-    
-    ans = input("Answer with  'y'  or  'n'\nDo you have/feel "+str(ques)[1:-1].strip()+" : ")
+q_index = 0
+for r, v in listofAllsymp.items():
+    ns = 0
+    ans = input("Answer with  'y'  or  'n'\nDo you have/feel "+str(r)+" : ")
     print()
-    if ans ==  "y": #replace this with while loop
-
-        for i in listAllsymp:
-            for j in i:
-                print(j,str(ques)[1:-1],"xxxxxxxxxxx")
-                if j == str(ques)[1:-1]:
-                    j =  'yes'
-                    print(j,"--------------")
-
-
-        # find index of MainSymp(deepCough) from listAllsymp, pick Allsymptoms; insert Allsymtom to QuestionList
-        for MainSymp in listAllsymp:
-            print(MainSymp[0], str(ques)[2:-2])
-
-            if MainSymp[0] == str(ques)[2:-2]:
-                ns = 1
-
-                for normSymp in MainSymp:
-                    try:
-                        if ns < len(MainSymp):
-                            allsymp = listAllsymp[eachdataset][ns]
-                            ns = ns + 1
-                            index = quesList.index(aaa)  # index of MainSymp
-                            print(ms)
-                            # insert after index of MainSymp, Allsympton of MainSymp
-                            quesList.insert(index+1, allsymp)
-                            print("===== symp added to questionlist", ns)
-                            # print("ms ki iteration", ms)
-                            ms = ms + 1
-                    except:
-                        pass
-                print("---finalilist-----", quesList, "-----------")
-                eachdataset = eachdataset + 1
-                print(eachdataset, "Allsymplist")
-
-            else:
-                pass
-    else:
-        pass
-
-
-# count of symptom listAllsymp
-# print(len(listAllsymp[0]))
-# only symptoms
-# i = 0
-# for ms in listAllsymp:
-#     print(listAllsymp[i][1:])
-#     i = i+1
-
-
-# find index of MainSymp(deepCOugh) from listAllsymp, pick Allsymptoms; insert Allsymtom to QuestionList
-# ms = 0
-# for MainSymp in listAllsymp:
-#     print(MainSymp[0],str(quesList[ms])[2:-2])
-
-#     if MainSymp[0] == str(ques)[2:-2]:
-#         print(MainSymp, "------")
-#         ns = 1
-#         for normSymp in MainSymp:
-#             if ns < len(MainSymp):
-#                 allsymp = listAllsymp[0][ns]
-#                 ns = ns + 1
-#             # insert after index of MainSymp, Allsympton of MainSymp
-#                 quesList.insert(index+1, allsymp)
-#                 print("======yes symp added to question")
-
-#         print(quesList,"*****finallist*****")
-#     ms = ms + 1
+    if ans == "y":  # replace this with while loop
+        index = QuesList.index(r)
+        print(QuesList[q_index], r, "xxxxxx")
+        if QuesList[q_index] == r:
+            for NS in v:
+                if ns < len(v):
+                    QuesList.insert(index+1, v[ns])
+                    print(v[ns], ns, len(v))
+                    ns = ns + 1
+                    q_index = q_index + 1
+    q_index = q_index + 1
+print("All Questions Asked: ",QuesList)
+print()
