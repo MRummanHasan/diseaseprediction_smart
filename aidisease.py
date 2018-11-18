306a2cb588f589d7cd1d50d180628ae4a538967c
M Rumman Hasan <mrayyanhkhan@outlook.com>
2018-11-18 21:10:09 +0500

    organised code

--- aidisease.py
+++ aidisease.py
@@ -1,8 +1,10 @@
 import pymysql
 from scipy.spatial import distance
+import numpy
 user_location = [2, 3]
 user_price = 500
-print("<<< Welcome to the IntelliHealth >>>")
+print("<<< Welcome to the IntelliHealth >>>\n")
+
 
 class Disease():
     def __init__(self, name, mainSymp, nSymp):
@@ -10,105 +12,6 @@ class Disease():
         self.mainSymp = mainSymp
         self.nSymp = nSymp
         self.nAns = []
-
-
-# fetch data from junction table
-# connection work
-conn = pymysql.connect(host='localhost', user='root',
-                       password='', db='HealthSystem')
-a = conn.cursor()
-# end connection work
-
-# Query
-# sql = 'SELECT D.disease_name, S.symptom_name FROM diseases AS D JOIN disease_symptoms_bridgetable AS DC ON D.disease_id = DC.disease_id JOIN symptoms AS S ON S.symptom_id = DC.symptom_id'
-# a.execute(sql)
-# data = a.fetchall()
-########################     OR    #####################
-# MainSymp : symp1, symp2....
-listofAllsymp = {
-    "Deep Cough": ["ear Pain", "sore Swelling", "change in Voice", "difficulty in breathing"],
-    "Jaw Pain": ["sore Throat", "teeth Loose", "painful Chewing"],
-    "MainSymp3": ["weakness", "fever", "cheek swelling", "other", "other2"],
-    'lung problems': ['cough with blood', 'shortness of breath'],
-    'skin problems': ['redness of face', 'moles on skin'],
-    'Diarrhea': ['watery stool', 'vomiting', 'abdominal cramps', 'belly pain'],
-    'Eating or Weight Problems': ['W', 'fever']
-}
-
-# Populate Disease objects
-dArray = []
-i = 0
-for k, v in listofAllsymp.items():
-    dArray.append(Disease("D"+str(i), k, v))
-    # print(dArray[i].name, dArray[i].mainSymp, dArray[i].nSymp)
-    i = i + 1
-# print()
-
-# ASK SYMPTOMS
-for d in dArray:
-    ans = input("Answer with  'y'  or  'n'\nDo you have/feel " +           str(d.name)+" > ")
-
-    if ans == "y":
-        for n in d.nSymp:
-            # print(n)
-            NSans = input("\nDo you have/feel "+str(n)+" : ")
-            if NSans == 'y':
-                d.nAns.append(NSans)
-
-# print details
-# for docDetail in dArray:
-#     print(docDetail.name, docDetail.mainSymp, docDetail.nSymp, docDetail.nAns)
-# print()
-
-# Calculate percentage of symptom occurance
-symp_percent = {}
-for dp in dArray:
-    ansSize = len(dp.nAns)
-    tsymp = len(dp.nSymp)
-    per = (ansSize / tsymp) * 100
-    symp_percent[dp.name] = per
-# print(symp_percent)
-
-Doc_to_dept = {
-    'ENT': ['D0', 'D1'],
-    'General': ['flu', 'D3'],
-    'Cancer': ['oral', 'D4'],
-    'Skin': ['D5', 'D6'],
-    'Dentist': ['D7', 'D8'],
-    'OPD': ['D0', 'D3'],
-    'Psychology': ['D2', 'D4']
-}
-
-# Check Department
-
-
-def check_depart(symp_percent, Doc_to_dept):
-    inverse = [(value, key) for key, value in symp_percent.items()]
-    highVal = max(inverse)[0]
-    # print(highVal, "high index")
-
-    # key at highest value
-    highValKey = list(symp_percent.keys())[
-        list(symp_percent.values()).index(highVal)]
-    # print(highValKey, "name")
-
-    for dtdk, dtdv in Doc_to_dept.items():
-        # print(dtdv)
-        if dtdv.__contains__(highValKey):
-            # print("success this depart: ")
-            return dtdk
-
-    return 'General'
-
-
-thisdepart = check_depart(symp_percent, Doc_to_dept)
-
-# print(thisdepart, "department for SQL")
-# thisdepart = "ENT"
-sql = 'SELECT doc_name,doc_dept,doc_TimeTo, doc_timeFrom,doc_location,doc_price FROM doctor WHERE doc_dept = '+'"'+thisdepart+'"'
-a.execute(sql)
-data = a.fetchall()
-print(data)
 
 
 class Doctor():
@@ -121,6 +24,69 @@ class Doctor():
         self.price = price
 
 
+# # SQL connection work
+conn = pymysql.connect(host='localhost', user='root',
+                       password='', db='HealthSystem')
+a = conn.cursor()
+# # end connection work
+
+# # Query to fetch data from juntion/bridge table in SQL
+# sql = 'SELECT Disease.disease_name, S.symptom_name FROM diseases AS Disease JOIN disease_symptoms_bridgetable AS DC ON Disease.disease_id = DC.disease_id JOIN symptoms AS S ON S.symptom_id = DC.symptom_id'
+# a.execute(sql)
+# # data = a.fetchall()
+########################     OR   use this dummy data #####################
+# # MainSymp : symp1, symp2....
+listofAllsymp = {
+    "Deep Cough": ["ear Pain", "sore Swelling", "change in Voice", "difficulty in breathing"],
+    "Jaw Pain": ["sore Throat", "teeth Loose", "painful Chewing"],
+    "MainSymp3": ["weakness", "fever", "cheek swelling", "other", "other2"],
+    'lung problems': ['cough with blood', 'shortness of breath'],
+    'skin problems': ['redness of face', 'moles on skin'],
+    'Diarrhea': ['watery stool', 'vomiting', 'abdominal cramps', 'belly pain'],
+    'Eating or Weight Problems': ['W', 'fever']
+}
+
+# # INPUR: Calculated-percentages of symptoms by User, Disease-to-Depart Data
+# # OUTPUT: Specific department name as patients symptoms
+# # FUNC: Check Department
+def check_depart(symp_percent, Disease_to_dept):
+    inverse = [(value, key) for key, value in symp_percent.items()]
+    highVal = max(inverse)[0]
+
+    # Key at highest value
+    highValKey = list(symp_percent.keys())[
+        list(symp_percent.values()).index(highVal)]
+
+    for dtdk, dtdv in Disease_to_dept.items():
+        if dtdv.__contains__(highValKey):
+            # print("success this depart: ")
+            return dtdk  # matched deaprtment
+
+    return 'General'
+
+# INPUT: nothing for now, BUT Raw Data from SQL
+# # OUTPUT: return Array of Objects of disease-details
+# # FUNC: Populate Disease from SQL-table to Disease-Objects
+def populateDisease():
+    disArray = []
+    i = 0
+    # print("Disease - MainSymptom - [Specific Symptoms]")
+    for k, v in listofAllsymp.items():
+        disArray.append(Disease("Disease"+str(i), k, v))
+        # print(disArray[i].name, disArray[i].mainSymp, disArray[i].nSymp)
+        i = i + 1
+    return disArray
+
+Disease_to_dept = {
+    'ENT': ['Disease0', 'Disease1'],
+    'General': ['Influenza', 'Disease3'],
+    'Cancer': ['Oral', 'Disease4'],
+    'Skin': ['Disease5', 'Disease6'],
+    'Dentist': ['Disease7', 'Disease8'],
+    'OPD': ['D0', 'Disease3'],
+    'Psychology': ['Disease2', 'Disease4']
+}
+
 # distance cost of hospitals
 dist_of_Hosp = {
     'Nazimabad Hospital': [5, 2],
@@ -129,29 +95,103 @@ dist_of_Hosp = {
     'Saddar Skin Hospital': [5, 5]
 }
 
-AllhospCord = []
-# Populate Doctor objects
-docObjArray = []
-for eachdoc in data:
-    docObjArray.append(
-        Doctor(eachdoc[0], eachdoc[1], eachdoc[2], eachdoc[3], eachdoc[4], eachdoc[5]))
-    #       name,  dept,  timeTo,  timeFrom,  location,   price
-    # print(eachdoc[0], eachdoc[1], eachdoc[2],          eachdoc[3], eachdoc[4], eachdoc[5])
+# # INPUR: Nothing
+# # OUTPUT: raw data of doctor
+# # FUNC: fetch data from SQL
+def fetchDatafromSQL(thisdepart):
+    sql = 'SELECT doc_name,doc_dept,doc_TimeTo, doc_timeFrom,doc_location,doc_price FROM doctor WHERE doc_dept = '+'"'+thisdepart+'"'
+    a.execute(sql)
+    data = a.fetchall()
+    print(data)
+    return data
 
-    # Contraints for Location
-    hosp_cord = dist_of_Hosp[eachdoc[4]]
-    # print(hosp_cord, "hosp cordinate")
+# # INPUT: Raw Data from SQL
+# # OUTPUT: return Array of Objects of doctors-details
+# # FUNC: Populate Doctor objects
+def populateDoctor(data):
+    doctorObjArray = []
+    for eachdoc in data:
+        doctorObjArray.append(
+            Doctor(eachdoc[0], eachdoc[1], eachdoc[2], eachdoc[3], eachdoc[4], eachdoc[5]))
+        ##       name,  dept,  timeTo,  timeFrom,  location,   price
+        print(eachdoc[0], eachdoc[1], eachdoc[2],
+              eachdoc[3], eachdoc[4], eachdoc[5])
 
-    user_points = (user_location[0], user_location[1], 0)
-    hosp_points = (hosp_cord[0], hosp_cord[1], 0)
-    dst = distance.euclidean(user_points, hosp_points)
-    # print(dst, "dsit from hosp")
-    AllhospCord.append(dst)
+    return doctorObjArray
+
+# # INPUR: obj-array of disease
+# # OUTPUT: nothing
+# # FUNC: print details of disease
+def printDisease(diseaseObjArray):
+    for diseaseDetails in diseaseObjArray:
+        print(diseaseDetails.name, diseaseDetails.mainSymp,
+              diseaseDetails.nSymp, diseaseDetails.nAns)
     print()
 
-try:
-    minVal = min(AllhospCord)
-    indexof_hospCord = AllhospCord.index(minVal)
-    print("Nearest Hospital is: ", docObjArray[indexof_hospCord].location)
-except:
-    print("Please visit your nearest Hospital")
+
+# # INPUR: distance cost of hospitals, Objedct array of doctors
+# # OUTPUT: print Nearest Hospital Name
+# # FUNC: search for nearest hospital to the patient
+def findNearestHosp(dist_of_Hosp, doctorObjArray):
+    for eachdoc in doctorObjArray:
+        AllhospCordinates = []
+        # Contraints for Location
+        hosp_cord = dist_of_Hosp[eachdoc.location]
+        # print(hosp_cord, "hosp cordinate")
+
+        user_points = (user_location[0], user_location[1], 0)
+        hosp_points = (hosp_cord[0], hosp_cord[1], 0)
+        dst = distance.euclidean(user_points, hosp_points)
+        # print(dst, "dsit from hosp")
+        AllhospCordinates.append(dst)
+
+    print()
+    try:
+        minVal = min(AllhospCordinates)
+        indexof_hospCord = AllhospCordinates.index(minVal)
+        print("Nearest Hospital is: ",
+              doctorObjArray[indexof_hospCord].location)
+    except:
+        print("Please visit your nearest Hospital")
+
+
+###################################################################
+# # STEP: 1
+diseaseObjArray = populateDisease()
+
+# # STEP: 2
+# # ASK SYMPTOMS
+for d in diseaseObjArray:
+    ans = input("Answer with 'y' or 'n'\nDo you have/feel " +
+                str(d.mainSymp)+" >>> ")
+    if ans == "y":
+        for n in d.nSymp:
+            NSans = input("\nDo you have/feel "+str(n)+" : ")
+            if NSans == 'y':
+                d.nAns.append(NSans)
+
+# # STEP: 3
+# Calculate percentage of symptom occurance
+symp_percent = {}
+for dispercent in diseaseObjArray:
+    ansSize = len(dispercent.nAns)
+    tsymp = len(dispercent.nSymp)
+    per = (ansSize / tsymp) * 100
+    symp_percent[dispercent.name] = per
+# print(symp_percent)
+
+
+# # STEP: 4
+thisdepart = check_depart(symp_percent, Disease_to_dept)
+
+# # STEP: 5
+# # Fetch data as per user
+data = fetchDatafromSQL(thisdepart)
+doctorObjArray = populateDoctor(data)
+
+# # STEP: 6
+# # Tell nearest hospital
+findNearestHosp(dist_of_Hosp, doctorObjArray)
+
+
+printDisease(diseaseObjArray)
